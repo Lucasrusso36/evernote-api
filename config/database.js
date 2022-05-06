eb19e7f8137f8d65951d7ee6e878c6920e0b2e40
Lucasrusso36 <lucaspidossantos@gmail.com>
2022-05-06 19:01:49 -0300

    database

--- config/database.js
+++ config/database.js
@@ -6,7 +6,6 @@ const MONGO_URL = process.env.MONGO_URL;
 mongoose.connect(MONGO_URL, { 
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true
 
 }).then(() =>  console.log('connection succesful'))
   .catch((err) => console.error(err));
