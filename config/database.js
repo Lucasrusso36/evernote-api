e1d4da0f5b035180d38c7d77525c1ff2558bb15f
Lucasrusso36 <lucaspidossantos@gmail.com>
2022-05-06 18:53:53 -0300

    first deploy

--- config/database.js
+++ config/database.js
@@ -6,6 +6,7 @@ const MONGO_URL = process.env.MONGO_URL;
 mongoose.connect(MONGO_URL, { 
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true
 
 }).then(() =>  console.log('connection succesful'))
   .catch((err) => console.error(err));
