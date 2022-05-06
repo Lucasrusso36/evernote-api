1c574bed58bff54e10cd98888d1cd6de3e87dd81
Lucasrusso36 <lucaspidossantos@gmail.com>
2022-05-06 19:08:43 -0300

    bug datab

--- config/database.js
+++ config/database.js
@@ -3,10 +3,10 @@ mongoose.Promise = global.Promise;
 
 require('dotenv').config();
 const MONGO_URL = process.env.MONGO_URL;
-  mongoose.connect(URI, {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true 
- }, err => {
-    if(err) throw err;
-    console.log('Connected to MongoDB!!!')
- })
+mongoose.connect(MONGO_URL, { 
+  useFindAndModify: false, 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+
+}).then(() =>  console.log('connection succesful'))
+  .catch((err) => console.error(err));
